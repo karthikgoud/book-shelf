e5b6311403b7ce8085bfb77719f85453a2b0b572
karthikgoud <karthikrv09@gmail.com>
2023-06-17 22:27:40 +0530

    updated data

--- src/constants/books.js
+++ src/constants/books.js
@@ -13,13 +13,13 @@ export const booksData = [
     id: 2,
     title: "IKIGAI",
     bookUrl:
-      "https://m.media-amazon.com/images/I/51yvBs8kAcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "https://m.media-amazon.com/images/I/41k8aHbVSBL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     author: "Scott Card",
     category: "current",
   },
   {
     id: 3,
-    title: "Enders Game",
+    title: "Robodog",
     bookUrl:
       "https://m.media-amazon.com/images/I/51yvBs8kAcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     author: "Scott Card",
@@ -37,7 +37,7 @@ export const booksData = [
   },
   {
     id: 5,
-    title: "Harry potter",
+    title: "THE ABYSS",
     bookUrl:
       "https://m.media-amazon.com/images/I/51SkIDTd9rL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     author: "JK Rowling",
@@ -55,9 +55,8 @@ export const booksData = [
   // ----------------------------------read
   {
     id: 7,
-    title: "Mocking Bird",
-    bookUrl:
-      "https://m.media-amazon.com/images/I/51Z9p5AecCL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "VAJPAYEE",
+    bookUrl: "https://m.media-amazon.com/images/I/41b87IOB+LL._SY346_.jpg",
     author: "Harper Lee",
     category: "read",
   },
@@ -71,9 +70,9 @@ export const booksData = [
   },
   {
     id: 9,
-    title: "Mocking Bird",
+    title: "Dust Child",
     bookUrl:
-      "https://m.media-amazon.com/images/I/51Z9p5AecCL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "https://m.media-amazon.com/images/I/51yVNfxbQaL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     author: "Harper Lee",
     category: "read",
   },
